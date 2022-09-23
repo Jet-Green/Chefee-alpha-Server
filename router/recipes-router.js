692d3103ff4bf1289b02755d8f2b7741176e9806
Jet-Green <grishadzyin@gmail.com>
2022-09-23 21:37:38 +0400

    add blocking like if no auth

--- router/recipes-router.js
+++ router/recipes-router.js
@@ -2,10 +2,12 @@ const Router = require('express').Router
 const router = Router()
 const recipesController = require('../controllers/recipes-controller')
 
+const authMiddleware = require('../middleware/auth-middleware')
+
 router.get('/get-all', recipesController.getAll)
 router.get('/get', recipesController.getById)
 router.get('/get-by-str', recipesController.getAll)
-router.post('/change-rating', recipesController.changeRating)
+router.post('/change-rating', authMiddleware, recipesController.changeRating)
 // использовать это, чтобы добавить тестовые рецепты
 // router.get('/test-insert', recipesController.insertOne)
 // router.get('/test-delete-all', recipesController.deleteAll)
